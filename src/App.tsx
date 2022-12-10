a77af7c92a5bfc9e369e4317c7bcbabd5ff47616
Dluter <tom230023@gmail.com>
2022-12-10 23:38:34 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 const getRandomColor = () => {
-  const digits = [
-    ...[...Array(16)].map((_, i) =>
-      i < 10 ? `${i}` : String.fromCharCode("A".charCodeAt(0) + i - 10)
-    ),
-  ];
-
+  const digits = [...Array(16)].map((_, i) =>
+    i < 10 ? `${i}` : String.fromCharCode("A".charCodeAt(0) + i - 10)
+  );
   const color = [...Array(6)]
     .map(() => digits[Math.floor(Math.random() * digits.length)])
     .join("");
